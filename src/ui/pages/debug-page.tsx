45d0fa4ca6343443fbf51095bc5dbb1c81cc8903
Arturas <a.molcanovas@gmail.com>
2022-11-27 16:03:14 +0000

    chore: Add item lookup to debug page

--- src/ui/pages/debug-page.tsx
+++ src/ui/pages/debug-page.tsx
@@ -1,13 +1,21 @@
-import {startCase} from 'lodash-es';
-import type {Currency, Game, Skill} from 'melvor';
+import {useComputed, useSignal} from '@preact/signals';
+import {pick, startCase, stubFalse} from 'lodash-es';
+import type {Currency, EquipmentItem, Game, Item, Skill} from 'melvor';
 import type {TypedKeys} from 'mod-util/typed-keys';
 import type {VNode} from 'preact';
+import {Fragment} from 'preact';
+import {memo} from 'preact/compat';
+import {useCallback, useState} from 'preact/hooks';
 import {ACTION_REGISTRY} from '../../lib/registries/action-registry.mjs';
 import {TRIGGER_REGISTRY} from '../../lib/registries/trigger-registry.mjs';
 import WorkflowRegistry from '../../lib/registries/workflow-registry.mjs';
-import {BlockDiv} from '../components/block';
+import {EMPTY_ARR} from '../../lib/util.mjs';
+import RenderMediaEdit from '../../option-types/media-item/render-media-edit';
+import type {MediaItemNodeOption} from '../../public_api';
+import {BlockDiv, BorderedBlock} from '../components/block';
 import Btn from '../components/btn';
-import autoId from '../id-gen.mjs';
+import {ProvideNodeValidationCtx} from '../components/workflow-editor/render-node-option/node-option-validation-ctx';
+import autoId from '../util/id-gen.mjs';
 
 export const DEBUG_PAGE_ID = autoId();
 
@@ -23,9 +31,92 @@ export default function DebugPage(): VNode {
         <GetCurrency path={'gp'}/>
         <GetCurrency path={'slayerCoins'}/>
       </div>
+      <ItemLookup/>
     </BlockDiv>
   );
 }
+
+const ItemLookup = (() => {
+  type TItemFmt = Pick<Item, 'category' | 'name' | 'id' | 'media' | 'type'>
+    & Partial<Pick<EquipmentItem, 'validSlots' | 'tier'>>;
+
+  const opt: MediaItemNodeOption = {
+    label: '.',
+    localID: '.',
+    registry: 'items',
+    type: 'MediaItem',
+  };
+
+  const RenderItemFmt = memo<{item: TItemFmt}>(
+    ({item}): VNode => {
+      const qty = useSignal('');
+      const onChange = useCallback((e: Event): void => {
+        qty.value = (e.target as HTMLInputElement).value;
+      }, EMPTY_ARR);
+
+      const grant = useCallback(() => {
+        const num = parseInt(qty.peek());
+        if (!isNaN(num)) {
+          game.bank.addItemById(item.id, num);
+        }
+      }, [item.id]);
+
+      return (
+        <Fragment>
+          <pre class={'text-light'}>{JSON.stringify(item, null, 2)}</pre>
+          <div class={'input-group'}>
+            <input placeholder={'Count'}
+              class={'form-control form-control-sm'}
+              type={'number'}
+              value={qty.value}
+              onInput={onChange}/>
+            <Btn onClick={grant} kind={'primary'}>{'Get'}</Btn>
+          </div>
+        </Fragment>
+      );
+    },
+    ({item: a}, {item: b}) => a?.id === b?.id
+  );
+
+  const formatItem = (item: Item): TItemFmt => pick(item, [
+    'name',
+    'id',
+    'category',
+    'tier',
+    'type',
+    'validSlots',
+    'media',
+  ]) as TItemFmt;
+
+  const ItemLookupInner = (): VNode => {
+    const [item, setItem] = useState<Item | undefined>(undefined);
+
+    return (
+      <Fragment>
+        <RenderMediaEdit value={item}
+          option={opt}
+          otherValues={{item}}
+          onChange={setItem as (v?: Item) => void}/>
+        {item && <RenderItemFmt item={formatItem(item)}/>}
+      </Fragment>
+    );
+  };
+
+  return (): VNode => {
+    const errors = useSignal(EMPTY_ARR);
+    const touched = useSignal(false);
+    const invalid = useComputed(stubFalse);
+
+    return (
+      <BorderedBlock kind={'woodcutting'} size={1}>
+        <div class={'font-size-sm font-w600'}>{'Item lookup'}</div>
+        <ProvideNodeValidationCtx errors={errors} touched={touched} invalid={invalid}>
+          <ItemLookupInner/>
+        </ProvideNodeValidationCtx>
+      </BorderedBlock>
+    );
+  };
+})();
 
 function PrintLog(): VNode {
   return (
