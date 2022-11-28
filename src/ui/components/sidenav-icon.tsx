92309f9584358f2d466509c9719de6616a4c27b4
Arturas <a.molcanovas@gmail.com>
2022-11-28 20:10:46 +0000

    fix: Fix sidenav icon getting stuck on "play"
    Closes #54

--- src/ui/components/sidenav-icon.tsx
+++ src/ui/components/sidenav-icon.tsx
@@ -1,7 +1,9 @@
-import {distinctWithInitial} from '@aloreljs/rxutils/operators';
+import {stubFalse} from 'lodash-es';
 import {memo} from 'preact/compat';
 import {useEffect, useState} from 'preact/hooks';
-import {map, skip} from 'rxjs';
+import type {Observable} from 'rxjs';
+import {concat, distinctUntilChanged, last, map, of, switchMap, takeWhile} from 'rxjs';
+import {WorkflowEventType} from '../../lib/execution/workflow-event.mjs';
 import WorkflowRegistry from '../../lib/registries/workflow-registry.mjs';
 import {EMPTY_ARR} from '../../lib/util.mjs';
 import {PauseSvg, PlaySvg} from './svg';
@@ -11,9 +13,21 @@ const SidenavIcon = memo(() => {
   useEffect(() => {
     const sub = WorkflowRegistry.inst.primaryExecution$
       .pipe(
-        map(Boolean),
-        distinctWithInitial(running),
-        skip(1)
+        switchMap((exec): Observable<boolean> => {
+          if (!exec) {
+            return of(false);
+          }
+
+          const src$ = exec
+            .pipe(
+              takeWhile(({type}) => type !== WorkflowEventType.WORKFLOW_COMPLETE),
+              last(null, null),
+              map(stubFalse)
+            );
+
+          return concat(of(true), src$);
+        }),
+        distinctUntilChanged()
       )
       .subscribe(setRunning);
     return () => {
