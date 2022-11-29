8c75e46b8f92d361169cfd91eba74b6413d766f8
Arturas <a.molcanovas@gmail.com>
2022-11-29 09:43:45 +0000

    perf: Minify polyfills

--- build/iife-wrap.mjs
+++ build/iife-wrap.mjs
@@ -1,8 +1,11 @@
-export default function iifeWrapPlugin({vars: varsArray}) {
+export default function iifeWrapPlugin({vars: varsArray, async}) {
   const vars = varsArray.join(',');
+  const renderChunk = async
+    ? (code => `await (async (${vars}) => {\n${code}\n})(${vars});`)
+    : (code => `((${vars}) => {\n${code}\n})(${vars});`);
 
   return {
     name: 'iife-wrap',
-    renderChunk: code => `await (async (${vars}) => {\n${code}\n})(${vars});`,
+    renderChunk,
   };
 }
--- rollup.config.js
+++ rollup.config.js
@@ -9,10 +9,142 @@ import {assetLoader} from "./build/asset-loader.mjs";
 import jsonPlugin from "@rollup/plugin-json";
 import cleanPlugin from "./build/clean-plugin.mjs";
 import scssLoader from "./build/scss-loader.mjs";
-import {join} from "node:path";
 
 const srcInclude = /src[\\/].+\.m?tsx?$/;
 const srcExclude = /node_modules[\\/]/;
+
+
+
+export default function (opts) {
+  const watch = opts.watch;
+  const prod = Boolean(getOpt(opts, 'prod'));
+
+  const baseOutput = {
+    dir: 'dist',
+    entryFileNames: '[name].mjs',
+    format: 'es',
+    preferConst: true,
+    preserveModules: false,
+    sourcemap: false,
+  };
+
+  return [
+    {
+      input: 'src/setup.tsx',
+      output: baseOutput,
+      plugins: [
+        cleanPlugin(),
+        mkNodeResolve(),
+        jsonPlugin({
+          compact: true,
+          include: '**/*.json',
+          namedExports: true,
+          preferConst: true,
+        }),
+        scssLoader({prod}),
+        typescript({exclude: srcExclude, include: srcInclude}),
+        replacePlugin({
+          exclude: srcExclude,
+          include: srcInclude,
+          preventAssignment: true,
+          values: {
+            'process.env.MELVOR_MOD_VERSION': JSON.stringify(process.env.MELVOR_MOD_VERSION || 'dev'),
+            'process.env.PRODUCTION': String(prod),
+          },
+        }),
+        assetLoader({
+          reg: /\.png$/,
+        }),
+        prod && iifeWrapPlugin({
+          async: true,
+          vars: [
+            'AltMagicConsumptionID',
+            'Array',
+            'Bank',
+            'Boolean',
+            'cdnMedia',
+            'console',
+            'Error',
+            'game',
+            'isNaN',
+            'JSON',
+            'Map',
+            'NamespacedObject',
+            'NamespaceRegistry',
+            'Number',
+            'Object',
+            'Promise',
+            'RegExp',
+            'SkillWithMastery',
+            'String',
+            'Symbol',
+            'Swal',
+            'TypeError',
+            'undefined',
+            'WeakMap',
+          ],
+        }),
+        modWrapPlugin({prod}),
+        prod && threadedTerserPlugin({
+          terserOpts: {
+            output: {
+              comments: false,
+            },
+          },
+        }),
+        copyPlugin({
+          copy: [
+            {
+              from: [
+                'manifest.json',
+                // 'public_api.d.ts',
+              ]
+            },
+            // {
+            //   from: 'adoptedStyleSheets.js',
+            //   opts: {
+            //     glob: {
+            //       cwd: join(process.cwd(), 'node_modules', 'construct-style-sheets-polyfill', 'dist'),
+            //     },
+            //   },
+            // }
+          ],
+          defaultOpts: {
+            glob: {cwd: 'src'},
+            emitNameKind: 'fileName',
+          },
+          watch,
+        }),
+      ],
+      watch: {
+        exclude: 'node_modules/**/*',
+      },
+    },
+    {
+      input: 'src/polyfills.mjs',
+      output: baseOutput,
+      plugins: [
+        mkNodeResolve(),
+        prod && iifeWrapPlugin({
+          async: false,
+          vars: [
+            'document',
+            'TypeError',
+            'WeakMap',
+            'window',
+          ],
+        }),
+        prod && threadedTerserPlugin({
+          terserOpts: {
+            output: {
+              comments: false,
+            },
+          },
+        }),
+      ],
+    },
+  ];
+}
 
 function getOpt(opts, opt) {
   if (opt in opts) {
@@ -23,119 +155,20 @@ function getOpt(opts, opt) {
   }
 }
 
-export default function (opts) {
-  const watch = opts.watch;
-  const prod = Boolean(getOpt(opts, 'prod'));
-
-  return {
-    input: 'src/setup.tsx',
-    output: {
-      dir: 'dist',
-      entryFileNames: 'setup.mjs',
-      format: 'es',
-      preferConst: true,
-      preserveModules: false,
-      sourcemap: false,
-    },
-    plugins: [
-      cleanPlugin(),
-      nodeResolve({
-        exportConditions: [
-          'es2015',
-          'module',
-          'import',
-          'default',
-        ],
-        mainFields: [
-          'es2015',
-          'esm2015',
-          'module',
-          'browser',
-          'main',
-        ],
-      }),
-      jsonPlugin({
-        compact: true,
-        include: '**/*.json',
-        namedExports: true,
-        preferConst: true,
-      }),
-      scssLoader({prod}),
-      typescript({exclude: srcExclude, include: srcInclude}),
-      replacePlugin({
-        exclude: srcExclude,
-        include: srcInclude,
-        preventAssignment: true,
-        values: {
-          'process.env.MELVOR_MOD_VERSION': JSON.stringify(process.env.MELVOR_MOD_VERSION || 'dev'),
-          'process.env.PRODUCTION': String(prod),
-        },
-      }),
-      assetLoader({
-        reg: /\.png$/,
-      }),
-      prod && iifeWrapPlugin({
-        vars: [
-          'AltMagicConsumptionID',
-          'Array',
-          'Bank',
-          'Boolean',
-          'cdnMedia',
-          'console',
-          'Error',
-          'game',
-          'isNaN',
-          'JSON',
-          'Map',
-          'NamespacedObject',
-          'NamespaceRegistry',
-          'Number',
-          'Object',
-          'Promise',
-          'RegExp',
-          'SkillWithMastery',
-          'String',
-          'Symbol',
-          'Swal',
-          'TypeError',
-          'undefined',
-          'WeakMap',
-        ],
-      }),
-      modWrapPlugin({prod}),
-      prod && threadedTerserPlugin({
-        terserOpts: {
-          output: {
-            comments: false,
-          },
-        },
-      }),
-      copyPlugin({
-        copy: [
-          {
-            from: [
-              'manifest.json',
-              // 'public_api.d.ts',
-            ]
-          },
-          {
-            from: 'adoptedStyleSheets.js',
-            opts: {
-              glob: {
-                cwd: join(process.cwd(), 'node_modules', 'construct-style-sheets-polyfill', 'dist'),
-              },
-            },
-          }
-        ],
-        defaultOpts: {
-          glob: {cwd: 'src'},
-          emitNameKind: 'fileName',
-        },
-        watch,
-      }),
+function mkNodeResolve() {
+  return  nodeResolve({
+    exportConditions: [
+      'es2015',
+      'module',
+      'import',
+      'default',
     ],
-    watch: {
-      exclude: 'node_modules/**/*',
-    },
-  };
+    mainFields: [
+      'es2015',
+      'esm2015',
+      'module',
+      'browser',
+      'main',
+    ],
+  });
 }
