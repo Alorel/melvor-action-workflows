1f33a97d0f087932f5c1d1f272b7c307e7c463e8
Arturas <a.molcanovas@gmail.com>
2023-01-20 21:32:32 +0000

    chore: Minor code style

--- src/ui/pages/help-page/youtube-embed.tsx
+++ src/ui/pages/help-page/youtube-embed.tsx
@@ -8,6 +8,7 @@ iframe{border:0;position:absolute;top:0;left:0;width:100%;height:100%}`);
 interface Props {
   vidId: string;
 }
+
 export default function YoutubeEmbed({vidId}: Props): VNode {
   return (
     <div>
